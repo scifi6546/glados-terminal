e6bc4d3d3047851114dabd79e8a1a90cf0bd21d4
scifi6546 <scifi6546@protonmail.com>
2018-06-16 14:14:37 -0800

    deleted not needed files

--- token.py
+++
@@ -1,8 +0,0 @@
-class token:
-    type=""
-    content=""
-    def __init__(self,type,content):
-        self.type=type
-        self.content=content
-    def __str__(self):
-        return "TOKEN   type: " + self.type + "    content: " + str(self.content)
